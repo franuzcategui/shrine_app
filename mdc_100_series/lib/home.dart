1bbecb7f11fac59a87e112d1295faeac8f9ab8f7
franuzcategui <45088588+franuzcategui@users.noreply.github.com>
2021-08-16 17:07:15 -0600

    Changes

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -13,6 +13,10 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'model/products_repository.dart';
+import 'model/product.dart';
 
 class HomePage extends StatelessWidget {
   // TODO: Make a collection of cards (102)
@@ -21,6 +25,56 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
+
+    List<Card> _buildGridCards(BuildContext context) {
+      List<Product> products = ProductsRepository.loadProducts(Category.all);
+
+      if (products.isEmpty) {
+        return const <Card>[];
+      }
+
+      final ThemeData theme = Theme.of(context);
+      final NumberFormat formatter = NumberFormat.simpleCurrency(locale: Localizations.localeOf(context).toString());
+
+            return products.map((product) {
+              return Card(
+                clipBehavior: Clip.antiAlias,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    AspectRatio(
+                      aspectRatio: 18.0 / 11.0,
+                      child: Image.asset(
+              product.assetName,
+                  package: product.assetPackage,
+              ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+              product.name,
+              style: theme.textTheme.headline6,
+              maxLines: 1,
+              ),
+                            SizedBox(height: 8.0),
+                            Text(
+              formatter.format(product.price),
+              style: theme.textTheme.subtitle2,
+              ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              );
+            }).toList();
+    }
+
     return Scaffold(
       // TODO: Add app bar (102)
       appBar: AppBar(
@@ -55,31 +109,7 @@ class HomePage extends StatelessWidget {
         crossAxisCount: 2,
         padding: EdgeInsets.all(16.0),
         childAspectRatio: 8.0 / 9.0,
-        children: <Widget>[
-          Card(
-            clipBehavior: Clip.antiAlias,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                AspectRatio(
-                  aspectRatio: 18.0 / 11.0,
-                  child: Image.asset('assets/diamond.png'),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Title'),
-                      SizedBox(height: 8.0),
-                      Text('Secondary Text'),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          )
-        ],
+        children: _buildGridCards(context)
       ),
       // TODO: Set resizeToAvoidBottomInset (101)
       resizeToAvoidBottomInset: false,
