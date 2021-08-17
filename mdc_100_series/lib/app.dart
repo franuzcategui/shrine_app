13816ecff1ad8b4f8c6b5122779fc6b6a0b0cbb6
franuzcategui <45088588+franuzcategui@users.noreply.github.com>
2021-08-17 13:37:47 -0600

    Changes

--- mdc_100_series/lib/app.dart
+++ mdc_100_series/lib/app.dart
@@ -67,5 +67,28 @@ ThemeData _buildShrineTheme() {
   );
 }
 
+TextTheme _buildShrineTextTheme(TextTheme base) {
+  return base.copyWith(
+    headline5: base.headline5!.copyWith(
+      fontSize: 18.0,
+    ),
+    headline6: base.headline6!.copyWith(
+      fontSize: 18.0,
+    ),
+    caption: base.caption!.copyWith(
+      fontWeight: FontWeight.w400,
+      fontSize: 14.0,
+    ),
+    bodyText1: base.bodyText1!.copyWith(
+      fontWeight: FontWeight.w500,
+      fontSize: 16.0,
+    ),
+  ).apply(
+    fontFamily: 'Rubik',
+    displayColor: kShrineBrown900,
+    bodyColor: kShrineBrown900,
+  );
+}
+
 // TODO: Build a Shrine Theme (103)
 // TODO: Build a Shrine Text Theme (103)
